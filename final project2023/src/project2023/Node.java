085cd95e6a58890b08a6459958746fad8b88f23e
Muhammad Faizan Raza <135647386+MuhammadFaizan-Raza@users.noreply.github.com>
2024-06-04 02:17:34 -0700

    Add files via upload

---
+++ final project2023/src/project2023/Node.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project2023;
+
+/**
+ *
+ * @author MUHAMMAD FAIZAN
+ */
+public class Node {
+//node structure
+    String data;
+    Node next10;
+    //*-*-*-*-*--Muhammad Faizan**-*-*-*-*-*-
+        Node next;
+}
